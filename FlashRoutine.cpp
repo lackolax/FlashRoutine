79b3aecda806b09dbac2a349b3641169b490ceb6
Tom Laxton <lackolaxton@gmail.com>
2020-07-17 12:39:23 +0100

    Create files

---
+++ FlashRoutine.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include "FlashRoutine.h"
+
+FlashRoutine::FlashRoutine(byte pin)
+{
+    pinMode(pin, OUTPUT);
+    _pin = pin;
+    lastIndex = 0;
+    lastFlash = 0;
+}
+
+void FlashRoutine::SetRoutine(int itemsInRoutine,int *routine)
+{
+    currentRoutine = routine;
+    routineSize = itemsInRoutine;
+    lastIndex = 0;
+    lastFlash = 0;
+    isStart = true;
+	manualOverride = false;
+}
+
+void FlashRoutine::SetOn()
+{
+	manualOverride = true;
+	manualState = true;
+}
+
+void FlashRoutine::SetOff()
+{
+	manualOverride = true;
+	manualState = false;
+}
+
+void FlashRoutine::Run()
+{
+	if(manualOverride)
+	{
+		digitalWrite(_pin, manualState);
+		return;
+	}
+    unsigned long currentMillis = millis();
+    if (isStart)
+    {
+        lastFlash = currentMillis;
+        lastIndex = 0;
+        digitalWrite(_pin, 0);
+        isStart = false;
+        return;
+    }
+
+    unsigned int interval = currentRoutine[lastIndex];
+
+    if ((currentMillis - lastFlash) > interval)
+    {
+        lastFlash = currentMillis;
+
+        digitalWrite(_pin, lastIndex % 2 == 0 ? 1 : 0);
+
+        if (lastIndex == (routineSize - 1))
+        {
+            lastIndex = 0;
+        }
+        else
+        {
+            lastIndex = lastIndex + 1;
+        }
+    }
+}
--- FlashRoutine.h
+++ FlashRoutine.h
@@ -1,4 +1,24 @@
 #ifndef FLASHROUTINE_H
 #define FLASHROUTINE_H
 
+class FlashRoutine
+{
+public:
+    FlashRoutine(byte pin);
+    void SetRoutine(int itemsInRoutine,int *routine);
+	void SetOn();
+	void SetOff();
+    void Run();
+
+private:
+    byte lastIndex;
+    unsigned long lastFlash;
+    int routineSize;
+    bool isStart;
+    byte _pin;
+	int *currentRoutine;
+	bool manualOverride;
+	bool manualState;
+};
+
 #endif
