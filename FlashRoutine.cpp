4ccea760123097c520ef05e684e6bdb79fd8a3eb
Tom Laxton <lackolaxton@gmail.com>
2020-07-26 12:47:42 +0100

    Add Brightness

--- FlashRoutine.cpp
+++ FlashRoutine.cpp
@@ -9,41 +9,51 @@ FlashRoutine::FlashRoutine(byte pin)
     lastFlash = 0;
 }
 
-void FlashRoutine::SetRoutine(int itemsInRoutine,int *routine)
+void FlashRoutine::SetBrightness(byte brightness)
+{
+    _brightness = brightness;
+}
+
+void FlashRoutine::SetRoutine(int itemsInRoutine, int *routine)
 {
     currentRoutine = routine;
     routineSize = itemsInRoutine;
     lastIndex = 0;
     lastFlash = 0;
     isStart = true;
-	manualOverride = false;
+    manualOverride = false;
 }
 
 void FlashRoutine::SetOn()
 {
-	manualOverride = true;
-	manualState = true;
+    manualOverride = true;
+    manualState = true;
 }
 
 void FlashRoutine::SetOff()
 {
-	manualOverride = true;
-	manualState = false;
+    manualOverride = true;
+    manualState = false;
+}
+
+void FlashRoutine::SetState(bool on)
+{
+    analogWrite(_pin, on ? map(_brightness, 0, 100, 0, 255) : 0);
 }
 
 void FlashRoutine::Run()
 {
-	if(manualOverride)
-	{
-		digitalWrite(_pin, manualState);
-		return;
-	}
+    if (manualOverride)
+    {
+        SetState(manualState);
+        return;
+    }
     unsigned long currentMillis = millis();
     if (isStart)
     {
         lastFlash = currentMillis;
         lastIndex = 0;
-        digitalWrite(_pin, 0);
+        SetState(0);
         isStart = false;
         return;
     }
@@ -54,7 +64,7 @@ void FlashRoutine::Run()
     {
         lastFlash = currentMillis;
 
-        digitalWrite(_pin, lastIndex % 2 == 0 ? 1 : 0);
+        SetState(lastIndex % 2 == 0 ? 1 : 0);
 
         if (lastIndex == (routineSize - 1))
         {
--- FlashRoutine.h
+++ FlashRoutine.h
@@ -5,20 +5,23 @@ class FlashRoutine
 {
 public:
     FlashRoutine(byte pin);
-    void SetRoutine(int itemsInRoutine,int *routine);
-	void SetOn();
-	void SetOff();
+    void SetRoutine(int itemsInRoutine, int *routine);
+    void SetBrightness(byte brightness);
+    void SetState(bool on);
+    void SetOn();
+    void SetOff();
     void Run();
 
 private:
     byte lastIndex;
+    byte _brightness;
     unsigned long lastFlash;
     int routineSize;
     bool isStart;
     byte _pin;
-	int *currentRoutine;
-	bool manualOverride;
-	bool manualState;
+    int *currentRoutine;
+    bool manualOverride;
+    bool manualState;
 };
 
 #endif
